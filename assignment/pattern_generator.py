fb5fd105836acef1267c987649f06bbbf072ee03
LucasRWoodmender <lrw84@drexel.edu>
2026-04-15 17:24:48 -0400

    Update pattern_generator.py

--- assignment/pattern_generator.py
+++ assignment/pattern_generator.py
@@ -5,7 +5,7 @@ cmds.file(new=True,force=True)
 
 def generate_pattern():
    
-#The informations on the 5 by 5 grid that is trying to be made with in the scene.
+#The informations on the 5 by 5 grid that is trying to be made with in the scene so I can use the .
     num_rows = 5
     num_cols = 5
     spacing = 3.0
@@ -27,40 +27,15 @@ def generate_pattern():
                 name = cmds.polyCylinder (name=box,radius=radius, height=height)[0]
             elif (row + col) % 3 == 0:
                 name = cmds.polyCube (name=box,width=width,height=height,depth=depth)[0]
+#This changes the shape to a sphere if nither of the other statements happen. this allows for all shapes that should be made will be made.
             else:
                 name = cmds.polySphere (name=box,radius=radius)[0]
+            
+# This allow for the different shapes to all move in the same way so that they all still make a 5 by 5 grid. 
             cmds.move(box_x,0,box_z,box)
-# HINT -- your loop structure should look something like this:
-#
-#   for row in range(num_rows):
-#       for col in range(num_cols):
-#           # Calculate position
-#           x_pos = col * spacing
-#           z_pos = row * spacing
-#
-#           # TODO: Add a conditional here that changes something
-#           # based on row, col, or (row + col).
-#           # For example:
-#           #   if (row + col) % 2 == 0:
-#           #       create a cube
-#           #   else:
-#           #       create a sphere
-
-#
-#           # TODO: Create the object using cmds.polyCube(), etc.
-#
-#           # TODO: Position the object using cmds.move().
-#
-#           # TODO: (Optional) Vary the scale using cmds.scale().
 
 
-
-
-# ---------------------------------------------------------------------------
-# Run the generator
-# ---------------------------------------------------------------------------
 generate_pattern()
 
-# Frame everything in the viewport.
 cmds.viewFit(allObjects=True)
 print("Pattern generated successfully!")
